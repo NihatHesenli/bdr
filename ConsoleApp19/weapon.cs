b6c4b3fb5229879424d59034e9b4e306affb97e4
Sad123iq <tu826UPXV@code.edu.az>
2024-04-02 09:33:11 +0400

    first

---
+++ ConsoleApp19/weapon.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    internal class weapon
+    {
+        public double CapacityBullet;
+        public double CurrentBullet;
+        public bool FireMood;
+         
+
+        public void Shoot()
+        {
+            if (FireMood == false)
+            {
+                Console.WriteLine("tekli");
+
+            }
+            else
+            {
+                Console.WriteLine("avtomatik");
+
+            }
+        }
+        public void GetRemainBulletcount()
+        {
+
+            CapacityBullet-=(double)CurrentBullet;
+        }
+        public void ChangeFireMood();
+
+    }
+}
